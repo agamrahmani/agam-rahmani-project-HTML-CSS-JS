9d77770c7f605d5f71e23acc0f825ad13485de52
agam <rahmaniagam2904@gmail.com>
2024-02-14 21:45:24 +0200

    added

---
+++ script.js
@@ -0,0 +1,13 @@
+function showSideBar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+
+}
+function hideSideBar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
+
+function cv() {
+    alert('קורות החיים יעלו לאתר בהמשך');
+}
